e4b500c3a7d129dc78e403d4fc24aa673f7a4774
Bazara Jackson <pasaseludko4@gmail.com>
2025-09-13 15:59:10 +0300

    Delete 15.cxx

--- 15.cxx
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main()
-{
-	int a,b,c,d;
-	std::cout<<"Введите первое число: ",std::cin>>a,std::cout<<"Введите второе число: ",std::cin>>b,std::cout<<"Введите третье число: ",std::cin>>c,std::cout<<"Введите четвëртое число: ",std::cin>>d,std::cout<<a<<" "<<b<<" "<<c<<" "<<d;
-}
