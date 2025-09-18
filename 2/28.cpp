b0aade35ae4af8549c631a6c74582fa4d49b509d
Bazara Jackson <pasaseludko4@gmail.com>
2025-09-18 16:21:00 +0300

    Create 28.cpp

---
+++ 2/28.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    float a,b;
+    std::cout<<"Введите объём: ",std::cin>>a,std::cout<<"Введите массу тела: ",std::cin>>b;
+    std::cout<<"Плотность равна: "<<b/a<<"\n";
+}
