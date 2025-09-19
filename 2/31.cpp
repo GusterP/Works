fbe911cf91b85c6b9090f447c5a91b41f231f3da
Bazara Jackson <pasaseludko4@gmail.com>
2025-09-19 21:09:45 +0300

    Create 31.cpp

---
+++ 2/31.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	float a,b;
+	std::cout<<"Дан треугольник\nПервый катет равен: ",std::cin>>a,std::cout<<"Второй катет равен: ",std::cin>>b;
+	std::cout<<"Гипотенуза равна: "<<sqrt(pow(a,2)+pow(b,2));
+}
