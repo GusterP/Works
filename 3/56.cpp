2a29e36c063f38ec368ff638376fd3bcedc4d75f
Bazara Jackson <pasaseludko4@gmail.com>
2025-10-09 11:24:30 +0300

    Create 56.cpp

---
+++ 3/56.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	float a;
+	std::cout<<"Введите количество киллограм: ";
+	std::cin>>a;
+	std::cout<<a<<" киллограм - это "<<a/100<<" центнеров";
+}
