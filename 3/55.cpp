49d5efad61cd3c71d9718222de504a6e9819098e
Bazara Jackson <pasaseludko4@gmail.com>
2025-10-09 11:20:59 +0300

    Create 55.cpp

---
+++ 3/55.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	float a;
+	std::cout<<"Введите количество сантиметров: ";
+	std::cin>>a;
+	std::cout<<a<<" сантиметров - это "<<a/100<<" метров";
+}
