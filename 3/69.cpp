86a354703479c1458a92d6ebd0f50dd3c157251b
Bazara Jackson <pasaseludko4@gmail.com>
2025-10-21 10:29:21 +0300

    Create 69.cpp

---
+++ 3/69.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	int a;
+	std::cout<<"Введите значение: ";
+	std::cin>>a;
+	std::cout<<(((a-1)/6)/9)%4+1<<" подъезд "<<((a-1)/6)%9+1<<" этаж "<<(a-1)%6+1<<" по счëту";
+}
