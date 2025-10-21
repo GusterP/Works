5c3975d9dc9c7cfa7d9c42219c6a4a38cb3e07bb
Bazara Jackson <pasaseludko4@gmail.com>
2025-10-21 11:07:09 +0300

    Create 78.cpp

---
+++ 3/78.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+	int a,b,c,d;
+	std::cout<<"Введите трехначное число: ";
+	std::cin>>a;
+	b = a/100;
+	c = a/10%10;
+	d = a%10;
+	std::cout<<b<<d<<c;
+}
