1eb5eaa0620514f786977c8026a7f49529849684
Bazara Jackson <pasaseludko4@gmail.com>
2025-11-27 09:12:03 +0300

    Create 108.cpp

---
+++ 4/108.cpp
@@ -0,0 +1,20 @@
+// 4.4
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Введите значение x: ";
+	cin>>x;
+	cout<<"Введите значение y: ";
+	cin>>y;
+	if (x<4)
+	{
+		cout<<I;
+	}
+	else
+	{
+		cout<<II;
+	}
+}
