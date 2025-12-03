4d160c768fd66f83877ddcf19d53c064282522de
Bazara Jackson <pasaseludko4@gmail.com>
2025-12-04 00:28:51 +0300

    Create 111.cpp

---
+++ 4/111.cpp
@@ -0,0 +1,26 @@
+// 4.7
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float x,k;
+	cout<<"Введите значение x: ";
+	cin>>x;
+	if (sin(x)>=0)
+	{
+		k = pow(x,2);
+	}
+	else
+	{
+		k = abs(x);
+	}
+	if (k<x)
+	{
+		cout<<k*x;
+	}
+	else
+	{
+		cout<<k+x;
+	}
+}
